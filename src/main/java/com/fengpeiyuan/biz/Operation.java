72748bc35004ebd60c0f37e87e36445b4ccd21d0
fengpeiyuan <fengpeiyuan@gmail.com>
2016-01-30 23:25:23 +0800

    deductbynumber

--- src/main/java/com/fengpeiyuan/biz/Operation.java
+++ src/main/java/com/fengpeiyuan/biz/Operation.java
@@ -4,6 +4,10 @@ import com.fengpeiyuan.util.BitUtil;
 import com.fengpeiyuan.dao.redis.shard.RedisShard;
 import org.apache.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Operation {
 	private RedisShard redisShard;
 	final static Logger logger = Logger.getLogger(Operation.class);
@@ -55,9 +59,11 @@ public class Operation {
 
 
 	/**
-	 * deduct
+	 * deduct 1
 	 * @param goodsId
-	 * @return position decuct
+	 * @return -2: exception
+	 * 		   -1:not enough stock
+	 * 		  >=0:position
 	 *
      */
 	public Integer stockDeductOne(String goodsId) {
@@ -75,7 +81,7 @@ public class Operation {
 			}
 		}catch (Exception t){
 			logger.error("error happend when deduct",t);
-			return -1;
+			return -2;
 		}
 		return offset;
 	}
@@ -84,22 +90,24 @@ public class Operation {
 	/**
 	 *
 	 * @param goodsId
-	 * @return
+	 * @return -2: key not exists
+	 * 		   -1: not enough stock
+	 *         >=0: position
      */
 	private static String shaDeductOneInLua;
-	private static String scriptDeductOneInLua = "local offset = redis.call('bitpos',KEYS[1],'1') local ret = redis.call('setbit',KEYS[1],offset,'0') if ret == 1 then return offset else return -1 end ";
-	/*eval "local offset = redis.call('bitpos',KEYS[1],'1') local ret = redis.call('setbit',KEYS[1],offset,'0') if ret ==1 then return offset else return -1 end " 1 a */
+	private static String scriptDeductOneInLua = "local isexist = redis.call('exists',KEYS[1]) if 0 == isexist then return -2 end  local offset = redis.call('bitpos',KEYS[1],'1') if offset==-1 then return -1 end  local ret = redis.call('setbit',KEYS[1],offset,'0') if ret == 1 then return offset else return -1 end ";
+	/*eval "local isexist = redis.call('exists',KEYS[1]) if 0 == isexist then return -2 end  local offset = redis.call('bitpos',KEYS[1],'1') if offset==-1 then return -1 end  local ret = redis.call('setbit',KEYS[1],offset,'0') if ret == 1 then return offset else return -1 end  " 1 a */
 
 	public Integer stockDeductOneInLua(String goodsId) {
 		Integer result = -1;
 		try {
 			if(null == Operation.shaDeductOneInLua){
-				if(this.getRedisShard().scriptExistsSingleShard(goodsId,"36c4584b8f58b58c1abf89a50fcf223dddf34537")){
-					Operation.shaDeductOneInLua = "36c4584b8f58b58c1abf89a50fcf223dddf34537";
+				if(this.getRedisShard().scriptExistsSingleShard(goodsId,"e01f375122b933f99b73ba39cf6324a154393de1")){
+					Operation.shaDeductOneInLua = "e01f375122b933f99b73ba39cf6324a154393de1";
 					result = ((Long)this.getRedisShard().evalshaSingleShard(goodsId,Operation.shaDeductOneInLua,1,goodsId)).intValue();
 				}else {
 					Operation.shaDeductOneInLua = this.getRedisShard().scriptLoadSingleShard(goodsId, Operation.scriptDeductOneInLua);
-					/*System.out.print(Operation.shaDeductOneInLua);*/
+					System.out.print(Operation.shaDeductOneInLua);
 					result = ((Long) this.getRedisShard().evalshaSingleShard(goodsId, Operation.shaDeductOneInLua, 1, goodsId)).intValue();
 				}
 			}else{
@@ -113,59 +121,45 @@ public class Operation {
 		return result;
 	}
 
-	class DeductResult {
-		private Integer posFrom = -1;
-		private Integer posTo = -1;
-
-		public Integer getPosFrom() {
-			return posFrom;
-		}
-
-		public void setPosFrom(Integer posFrom) {
-			this.posFrom = posFrom;
-		}
-
-		public Integer getPosTo() {
-			return posTo;
-		}
-
-		public void setPosTo(Integer posTo) {
-			this.posTo = posTo;
-		}
-
-
-	}
 
 	/**
 	 *
 	 * @param goodsId
 	 * @param number
-     * @return
+     * @return {-2}: invalid skuid or number(less than 0 or more then 5000)
+	 * 		   {}: empty table, not enough stock to deduct(number>remain)
      */
 	private static String shaDeductByNumberInLua;
-	private static String scriptDeductByNumberInLua = "";
+	private static String scriptDeductByNumberInLua = "local tonumber=tonumber   local t_pos={}  local isexist = redis.call('exists',KEYS[1]) if 0 == isexist then t_pos[1]=-2 return t_pos end  local number=tonumber(KEYS[2])  if number<=0 or number>5000 then t_pos[1]=-2 return t_pos end  local remain=redis.call('bitcount',KEYS[1])  if number>remain then t_pos[1]=-1 return t_pos end  local i=1 while i<=number do local offset=redis.call('bitpos',KEYS[1],'1') local ret = redis.call('setbit',KEYS[1],offset,'0') if ret ==1 then t_pos[i]=offset i=i+1 end end return t_pos";
+	/*eval "local tonumber=tonumber   local t_pos={}  local isexist = redis.call('exists',KEYS[1]) if 0 == isexist then t_pos[1]=-2 return t_pos end  local number=tonumber(KEYS[2])  if number<=0 or number>5000 then t_pos[1]=-2 return t_pos end  local remain=redis.call('bitcount',KEYS[1])  if number>remain then t_pos[1]=-1 return t_pos end  local i=1 while i<=number do local offset=redis.call('bitpos',KEYS[1],'1') local ret = redis.call('setbit',KEYS[1],offset,'0') if ret ==1 then t_pos[i]=offset i=i+1 end end return t_pos " 2 a 100 */
 
-	public DeductResult stockDeductByNumberInLua(String goodsId,Integer number) {
-		DeductResult result = new DeductResult();
+	public List<Integer> stockDeductByNumberInLua(String goodsId, Integer number) {
+		List<Integer> result;
+		List<Integer> ret = new ArrayList<Integer>();
 		try {
-			if(null == Operation.shaDeductOneInLua){
-				if(this.getRedisShard().scriptExistsSingleShard(goodsId,"")){
-					Operation.shaDeductOneInLua = "";
-//					result = ((Long)this.getRedisShard().evalshaSingleShard(goodsId,Operation.shaDeductByNumberInLua,2,goodsId,number.toString())).intValue();
+			if(null == Operation.shaDeductByNumberInLua){
+				if(this.getRedisShard().scriptExistsSingleShard(goodsId,"fcacd0b4a74789e3084c8d95f85ee428f98fe2df")){
+					Operation.shaDeductByNumberInLua = "fcacd0b4a74789e3084c8d95f85ee428f98fe2df";
+					result = (List)this.getRedisShard().evalshaSingleShard(goodsId,Operation.shaDeductByNumberInLua,2,goodsId,number.toString());
 				}else {
-					Operation.shaDeductOneInLua = this.getRedisShard().scriptLoadSingleShard(goodsId, Operation.scriptDeductByNumberInLua);
-					/*System.out.print(Operation.shaDeductOneInLua);*/
-//					result = ((Long) this.getRedisShard().evalshaSingleShard(goodsId, Operation.shaDeductByNumberInLua, 2, goodsId,number.toString())).intValue();
+					Operation.shaDeductByNumberInLua = this.getRedisShard().scriptLoadSingleShard(goodsId, Operation.scriptDeductByNumberInLua);
+					/*System.out.print(Operation.shaDeductByNumberInLua);*/
+					result = (List) this.getRedisShard().evalshaSingleShard(goodsId, Operation.shaDeductByNumberInLua, 2, goodsId,number.toString());
 				}
 			}else{
-//				result = ((Long)this.getRedisShard().evalshaSingleShard(goodsId,Operation.shaDeductByNumberInLua,2,goodsId,number.toString())).intValue();
+				result = (List)this.getRedisShard().evalshaSingleShard(goodsId,Operation.shaDeductByNumberInLua,2,goodsId,number.toString());
+			}
+
+			Iterator it=result.iterator();
+			while (it.hasNext()){
+				ret.add(((Long)it.next()).intValue());
 			}
 
 		}catch (Exception t){
 			logger.error("error happend when stockDeductByNumberInLua",t);
-			return result;
+			return ret;
 		}
-		return result;
+		return ret;
 	}
 
 
@@ -213,7 +207,7 @@ public class Operation {
 					result = ((Long)this.getRedisShard().evalshaSingleShard(goodsId,Operation.shaRefillByNumberInLua,2,goodsId,number.toString())).intValue();
 				}else {
 					Operation.shaRefillByNumberInLua = this.getRedisShard().scriptLoadSingleShard(goodsId, Operation.scriptRefillByNumberInLua);
-					/*System.out.print(Operation.shaRefillByNumberInLua);*/
+					System.out.print(Operation.shaRefillByNumberInLua);
 					result = ((Long) this.getRedisShard().evalshaSingleShard(goodsId, Operation.shaRefillByNumberInLua, 2, goodsId, number.toString())).intValue();
 				}
 			}else{
--- src/test/java/com.fengpeiyuan.biz/OperationTest.java
+++ src/test/java/com.fengpeiyuan.biz/OperationTest.java
@@ -4,6 +4,9 @@ package com.fengpeiyuan.biz;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -45,12 +48,42 @@ public class OperationTest {
     @Test
     public void testStockDeductOneInLua(){
         String goodsId = "a";
+        Integer num = 10;
         Operation operation = (Operation)context.getBean("operation");
+        Integer initRet = operation.stockInit(goodsId,num);
         Integer ret = operation.stockDeductOneInLua(goodsId);
         assertTrue(ret>=0);
+
+        Integer initRet2 = operation.stockInit(goodsId,0);
         Integer ret2 = operation.stockDeductOneInLua(goodsId);
-        assertTrue(ret2>=0);
+        assertTrue(ret2==-1);
+
+        Integer ret3 = operation.stockDeductOneInLua("@#$%");
+        assertTrue(ret3==-2);
     }
+
+    @Test
+    public void testStockDeductByNumberInLua(){
+        String goodsId = "a";
+        Integer num = 10;
+        Operation operation = (Operation)context.getBean("operation");
+        Integer initRet = operation.stockInit(goodsId,num);
+        List<Integer> ret = operation.stockDeductByNumberInLua(goodsId,2);
+        assertTrue(ret.size()==2);
+
+        operation.stockSendbackOne(goodsId,0);
+        List<Integer> retAgain = operation.stockDeductByNumberInLua(goodsId,2);
+        assertTrue(retAgain.get(0)==0&&retAgain.get(1)==2);
+
+        List<Integer> retAgain2 = operation.stockDeductByNumberInLua(goodsId,20);
+        assertTrue(retAgain2.get(0)==-1);
+
+        List<Integer> retAgain3 = operation.stockDeductByNumberInLua("@#$%",2);
+        assertTrue(retAgain3.get(0)==-2);
+
+
+    }
+
 
     @Test
     public void testStockRefillByNumberInLua(){
